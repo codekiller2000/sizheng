f024a3ddad4330d9db43ec9ffc2085387c255f14
codekiller2000 <50763599+codekiller2000@users.noreply.github.com>
2020-05-17 17:17:22 +0800

    Add files via upload

---
+++ weapp-wechat-sizheng/data/data_discovery.js
@@ -0,0 +1,112 @@
+/**
+ * Created by Rebecca_Han on 16/10/26.
+ */
+module.exports = {
+
+}
+
+var discovery= {
+    "id": 3,
+        "data": [
+        {
+            "question_id": 1,
+            "answer_id": 3,
+            "feed_source_id": 23,
+            "feed_source_name": "Rebecca1",
+            "feed_source_txt": "赞了回答1",
+            "feed_source_img": "../../images/icon1.jpeg",
+            "question": "我们为何要爱国？",
+            "answer_ctnt": "“我们之所以思考爱国主义，是因为国家不但保障了我们的安全、自由和生活，他还涉及到我们的价值观和世界观问题。 1949年10月1日，新中国宣布成立，这是用无数先辈的鲜血换来的，来之不易，这么多人为了国家做出了牺牲，因此我们必须深入思考那些把我们凝为一体、组成国家的理念...",
+            "good_num": "112",
+            "comment_num": "18"
+        },
+        {
+            "question_id": 2,
+            "answer_id": 25,
+            "feed_source_id": 24,
+            "feed_source_name": "Alex2",
+            "feed_source_txt": "回答了问题2",
+            "feed_source_img": "../../images/icon8.jpg",
+            "question": "如何评价周杰伦的「中文歌才是最屌的」的言论？",
+            "answer_ctnt": "不知道题主是否是学音乐的。 音乐有公认的经典，也有明显的流行趋势没有错。但归根结底，音乐是一种艺术，艺术是很主观的东西。跟画作一个道理，毕加索是大家，但很多人看不懂他的话，甚至觉得很难看...",
+            "good_num": "112",
+            "comment_num": "18"
+        },
+        {
+            "question_id": 3,
+            "answer_id": 61,
+            "feed_source_id": 25,
+            "feed_source_name": "George3",
+            "feed_source_txt": "赞了回答3",
+            "feed_source_img": "../../images/icon9.jpeg",
+            "question": "气象铁塔的辐射大吗？",
+            "answer_ctnt": "我不知道那个铁塔的情况，不过气象铁塔上会有一些测太阳辐射的设备，如果说辐射的话，太阳辐射那么多，大家赶紧躲进地底下呀~~~~~要不然辐射量这么大，会变异的呀~~~~",
+            "good_num": "112",
+            "comment_num": "18"
+        },
+        {
+            "question_id": 4,
+            "answer_id": 3,
+            "feed_source_id": 23,
+            "feed_source_name": "Rebecca4",
+            "feed_source_txt": "赞了回答4",
+            "feed_source_img": "../../images/icon1.jpeg",
+            "question": "我们为何要爱国？",
+            "answer_ctnt": "“我们之所以思考爱国主义，是因为国家不但保障了我们的安全、自由和生活，他还涉及到我们的价值观和世界观问题。 1949年10月1日，新中国宣布成立，这是用无数先辈的鲜血换来的，来之不易，这么多人为了国家做出了牺牲，因此我们必须深入思考那些把我们凝为一体、组成国家的理念...",
+            "good_num": "112",
+            "comment_num": "18"
+        },
+        {
+            "question_id": 5,
+            "answer_id": 25,
+            "feed_source_id": 24,
+            "feed_source_name": "Alex5",
+            "feed_source_txt": "回答了问题5",
+            "feed_source_img": "../../images/icon8.jpg",
+            "question": "如何评价周杰伦的「中文歌才是最屌的」的言论？",
+            "answer_ctnt": "不知道题主是否是学音乐的。 音乐有公认的经典，也有明显的流行趋势没有错。但归根结底，音乐是一种艺术，艺术是很主观的东西。跟画作一个道理，毕加索是大家，但很多人看不懂他的话，甚至觉得很难看...",
+            "good_num": "112",
+            "comment_num": "18"
+        },
+        {
+            "question_id": 6,
+            "answer_id": 61,
+            "feed_source_id": 25,
+            "feed_source_name": "George6",
+            "feed_source_txt": "赞了回答6",
+            "feed_source_img": "../../images/icon9.jpeg",
+            "question": "气象铁塔的辐射大吗？",
+            "answer_ctnt": "我不知道那个铁塔的情况，不过气象铁塔上会有一些测太阳辐射的设备，如果说辐射的话，太阳辐射那么多，大家赶紧躲进地底下呀~~~~~要不然辐射量这么大，会变异的呀~~~~",
+            "good_num": "112",
+            "comment_num": "18"
+        },
+        {
+            "question_id": 7,
+            "answer_id": 3,
+            "feed_source_id": 23,
+            "feed_source_name": "Rebecca7",
+            "feed_source_txt": "赞了回答7",
+            "feed_source_img": "../../images/icon1.jpeg",
+            "question": "我们为何要爱国？",
+            "answer_ctnt": "“我们之所以思考爱国主义，是因为国家不但保障了我们的安全、自由和生活，他还涉及到我们的价值观和世界观问题。 1949年10月1日，新中国宣布成立，这是用无数先辈的鲜血换来的，来之不易，这么多人为了国家做出了牺牲，因此我们必须深入思考那些把我们凝为一体、组成国家的理念...",
+            "good_num": "112",
+            "comment_num": "18"
+        },
+        {
+            "question_id": 8,
+            "answer_id": 25,
+            "feed_source_id": 24,
+            "feed_source_name": "Alex8",
+            "feed_source_txt": "回答了问题8",
+            "feed_source_img": "../../images/icon8.jpg",
+            "question": "如何评价周杰伦的「中文歌才是最屌的」的言论？",
+            "answer_ctnt": "不知道题主是否是学音乐的。 音乐有公认的经典，也有明显的流行趋势没有错。但归根结底，音乐是一种艺术，艺术是很主观的东西。跟画作一个道理，毕加索是大家，但很多人看不懂他的话，甚至觉得很难看...",
+            "good_num": "112",
+            "comment_num": "18"
+        }
+
+    ]
+
+}
+
+module.exports.discovery = discovery;
